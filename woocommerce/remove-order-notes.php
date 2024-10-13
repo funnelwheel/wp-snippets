e31aa9341e4982c1a05c2a2e7b2d0e6fd3d35682
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2024-10-13 16:49:37 +0530

    Update remove-order-notes.php

--- woocommerce/remove-order-notes.php
+++ woocommerce/remove-order-notes.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* Snippet Name:     Remove the Order Notes field section from the WooCommerce checkout.
+* Snippet Name:	Remove the Order Notes field section from the WooCommerce checkout.
 */
 add_filter( 'woocommerce_enable_order_notes_field', '__return_false', 9999 );
 add_filter( 'woocommerce_checkout_fields' , 'remove_order_notes' );
