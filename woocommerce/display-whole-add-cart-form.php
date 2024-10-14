341dc6ce3a1c7291007c3e1011e6959368d4d258
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2024-10-14 16:25:16 +0530

    Create display-whole-add-cart-form.php
    Snippet Name:	Display the Add Cart Form on WooCommerce Shop page for variable products.

---
+++ woocommerce/display-whole-add-cart-form.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Snippet Name:	Display the Add Cart Form on WooCommerce Shop page for variable products.
+*/
+add_action( 'woocommerce_after_shop_loop_item', 'display_whole_add_cart_form', 1 );
+function display_whole_add_cart_form() {
+   global $product;
+   if ( ! $product->is_purchasable() ) return;
+   remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+   add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_single_add_to_cart', 11 );
+}
