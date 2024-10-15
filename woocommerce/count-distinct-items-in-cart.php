3a3af7b043c24ac13f4d482f060a5fe8096231a0
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2024-10-15 15:29:46 +0530

    Update count-distinct-items-in-cart.php
    removed empty space

--- woocommerce/count-distinct-items-in-cart.php
+++ woocommerce/count-distinct-items-in-cart.php
@@ -2,7 +2,7 @@
 /**
 * Snippet Name:	Count the number of distinct items in the WooCommerce Cart.
 */
- add_filter( 'woocommerce_cart_contents_count', 'count_distinct_items_in_cart', 9999, 1 );
+add_filter( 'woocommerce_cart_contents_count', 'count_distinct_items_in_cart', 9999, 1 );
  
 function count_distinct_items_in_cart( $count ) {
    $count = count( WC()->cart->get_cart() );
