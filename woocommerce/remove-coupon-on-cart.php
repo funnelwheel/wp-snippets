fa758e9e84d97cd83a103ae0a4713d4e092fe6b4
Kishore Chandra Sahoo <babunsahoo@gmail.com>
2024-10-16 12:59:08 +0530

    Create remove-coupon-on-cart.php
    Remove the coupon from the WooCommerce cart

---
+++ woocommerce/remove-coupon-on-cart.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Snippet Name:	Remove the coupon from the WooCommerce cart.
+*/
+add_filter( 'woocommerce_coupons_enabled', 'remove_coupons_on_cart' );
+ 
+function remove_coupons_on_cart() {
+   if ( is_cart() ) return false;
+   return true;
+}
