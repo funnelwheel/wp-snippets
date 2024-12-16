dcfc4de99dc8af89947af1dbdc8439945cdea2b8
Kishore Chandra Sahoo <kishore@upnrunn.com>
2024-12-16 18:13:32 +0530

    Update sales-count.php

--- woocommerce/sales-count.php
+++ woocommerce/sales-count.php
@@ -1,12 +1,13 @@
 <?php
 /**
 * Snippet Name:	Fetch Product Sales Count
-* Don't forget to add pass the product id
+* Don't forget to add pass the product ID 
+* Use like this sales_count(123); // will return the sales count
 */
 function sales_count($product_id) {
-    // $product_id = 123; // Pass that id
     $product = wc_get_product( $product_id );
     $sales_count = $product->get_total_sales();
     
-    echo 'This product has been sold ' . $sales_count . ' times.';
+    // echo 'This product has been sold ' . $sales_count . ' times.';
+    return $sales_count;
 }
