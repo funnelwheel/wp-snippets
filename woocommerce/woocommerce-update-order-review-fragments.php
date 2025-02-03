85543fd723de2d6bf8f3b0602a0972226c2bf466
Kishore Chandra Sahoo <kishore@upnrunn.com>
2025-02-04 00:03:15 +0530

    Create woocommerce-update-order-review-fragments.php

---
+++ woocommerce/woocommerce-update-order-review-fragments.php
@@ -0,0 +1,23 @@
+add_filter( 'woocommerce_update_order_review_fragments', function ( $fragments ) {
+	if ( isset( $fragments['.woocommerce-checkout-payment'] ) && empty( $fragments['.woocommerce-checkout-payment'] ) ) {
+
+		ob_start();
+		if ( WC()->cart->needs_payment() ) {
+			$available_gateways = WC()->payment_gateways()->get_available_payment_gateways();
+			WC()->payment_gateways()->set_current_gateway( $available_gateways );
+		} else {
+			$available_gateways = array();
+		}
+		$checkout          = WC()->checkout();
+		$order_button_text = apply_filters( 'woocommerce_order_button_text', __( 'Place order', 'woocommerce' ) );
+		include TEMPLATE . '/checkout/payment.php';
+		$html = ob_get_clean();
+		if ( ! empty( $html ) ) {
+			$fragments['.woocommerce-checkout-payment'] = $html;
+		} else {
+			unset( $fragments['.woocommerce-checkout-payment'] );
+		}
+	}
+
+	return $fragments;
+}, 999 );
