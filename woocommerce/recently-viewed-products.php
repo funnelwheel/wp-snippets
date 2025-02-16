c75d9cf162e79f17e2c09b15f2b7d603e89c9e27
Kishore Chandra Sahoo <kishore@upnrunn.com>
2025-02-16 13:02:41 +0530

    Update recently-viewed-products.php

--- woocommerce/recently-viewed-products.php
+++ woocommerce/recently-viewed-products.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: Recently Viewed Products
- * Description: A plugin to display recently viewed products using WooCommerce's native query.
- * Version: 1.0.0
+ * Plugin Name: Recently Viewed Products with Email Notification
+ * Description: A plugin to display recently viewed products and send email notifications to users after a specific period.
+ * Version: 1.1.0
  * Author: Kishores
  * License: GPL3
  */
@@ -14,9 +14,7 @@ if ( ! class_exists( 'WooCommerce' ) ) {
 
 /**
  * Shortcode to display recently viewed products with support for dynamic attributes.
- * Usage: Once the plugin is activated, you can use the shortcode [recently_viewed_products] in any post, page, or widget.
- * e.g.: [recently_viewed_products per_page="6" orderby="price" order="asc" category="clothing" columns="3"]
- *
+ * Usage: [recently_viewed_products per_page="6" orderby="price" order="asc" category="clothing" columns="3"]
  */
 add_shortcode( 'recently_viewed_products', 'wc_recently_viewed_shortcode' );
 
@@ -115,3 +113,70 @@ function wc_custom_track_product_view() {
 }
 
 add_action( 'template_redirect', 'wc_custom_track_product_view', 20 );
+
+/**
+ * Check if 5 days have passed since the last product view and send an email.
+ */
+function wc_check_and_send_recently_viewed_email( $user_id, $viewed_products, $viewed_timestamps ) {
+    // Calculate the time difference (in seconds) from the last viewed product
+    $last_viewed_time = end( $viewed_timestamps );
+    $current_time = time();
+    $time_difference = $current_time - $last_viewed_time;
+
+    // Check if more than 5 days (432000 seconds) have passed and user has viewed at least 5 products
+    if ( $time_difference >= 432000 && sizeof( $viewed_products ) >= 5 ) {
+        // Send the email
+        wc_send_recently_viewed_email( $user_id, $viewed_products );
+    }
+}
+
+/**
+ * Send an email with the recently viewed products to the user.
+ */
+function wc_send_recently_viewed_email( $user_id, $viewed_products ) {
+    $user_info = get_userdata( $user_id );
+    $user_email = $user_info->user_email;
+
+    // Prepare the product details
+    $products = wc_get_products( array( 'include' => $viewed_products ) );
+    $product_list = '';
+    foreach ( $products as $product ) {
+        $product_list .= '<li><a href="' . get_permalink( $product->get_id() ) . '">' . $product->get_name() . '</a></li>';
+    }
+
+    // Email subject and body
+    $subject = 'Your Recently Viewed Products';
+    $message = '
+        <html>
+        <head>
+            <title>Your Recently Viewed Products</title>
+        </head>
+        <body>
+            <p>Hi ' . $user_info->first_name . ',</p>
+            <p>Here are the products you have recently viewed:</p>
+            <ul>' . $product_list . '</ul>
+            <p>We hope this helps you find what you are looking for!</p>
+        </body>
+        </html>
+    ';
+
+    // Send the email
+    wp_mail( $user_email, $subject, $message, array( 'Content-Type: text/html; charset=UTF-8' ) );
+}
+
+/**
+ * Track the user’s recently viewed products and send an email after 5 days.
+ */
+function wc_track_and_email_recently_viewed_products() {
+    if ( is_user_logged_in() ) {
+        $user_id = get_current_user_id();
+        $viewed_products = get_user_meta( $user_id, '_recently_viewed_products', true );
+        $viewed_timestamps = get_user_meta( $user_id, '_recently_viewed_product_timestamps', true );
+
+        // Check if it's time to send the email (5 days after the last viewed product)
+        wc_check_and_send_recently_viewed_email( $user_id, $viewed_products, $viewed_timestamps );
+    }
+}
+
+add_action( 'template_redirect', 'wc_track_and_email_recently_viewed_products', 20 );
+
